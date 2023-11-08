9ac7ecbde0563a3e96659670c58237951a7d536c
James Li <james.li@avivasolutions.nl>
2023-11-08 13:14:36 +0100

    Add example workflows

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          v0.0.1 Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
